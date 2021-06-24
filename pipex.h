5c97e43bf5907dd5ef4679343ff3000d5bbdb4e6
Amine Tahiri <atahiri@student.1337.ma>
2021-06-24 11:31:45 +0100

    okok

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: atahiri <atahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 09:32:40 by atahiri           #+#    #+#             */
-/*   Updated: 2021/06/24 10:01:28 by atahiri          ###   ########.fr       */
+/*   Updated: 2021/06/24 11:31:27 by atahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,14 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <string.h>
+
+typedef struct s_pipex {
+	int		fd[2];
+	char	*input;
+	char	**cmd1;
+	char	**cmd2;
+	char	*output;
+}				t_pipex;
 
 char	**ft_split(const char *ss, char c);
 int		ft_strlen(char *s);
